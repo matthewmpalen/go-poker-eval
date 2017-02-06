0dfdcea13a2a7806f3e64a7cbfb73d3e2d0e666a
Matt Palen <matthewmpalen@gmail.com>
2017-02-06 11:49:12 -0500

    Use iota for hand type enum

--- constants.go
+++ constants.go
@@ -29,15 +29,15 @@ const (
 )
 
 const (
-	StraightFlush = 1
-	FourOfAKind   = 2
-	FullHouse     = 3
-	Flush         = 4
-	Straight      = 5
-	ThreeOfAKind  = 6
-	TwoPair       = 7
-	OnePair       = 8
-	HighCard      = 9
+	StraightFlush = iota + 1
+	FourOfAKind
+	FullHouse
+	Flush
+	Straight
+	ThreeOfAKind
+	TwoPair
+	OnePair
+	HighCard
 )
 
 const (
